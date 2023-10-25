df7ab2ec9b0ef57d0e4084b4f2c894cb13646b37
Danishfa8 <114738545+Danishfa8@users.noreply.github.com>
2023-10-25 12:19:12 +0700

    Add files via upload

---
+++ latihan18/src/ti_regpagi/pkg22205043/latihan18/Kelipatan/TI_RegPagi22205043Latihan18Kelipatan.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ti_regpagi.pkg22205043.latihan18.Kelipatan;
+
+/*
+ *  Nama      : Danish Fadlan Azam
+ *  NIM       : 22205043
+ *  Prodi     : Teknik Informatika
+ *  Deskripsi : program ini untuk menghitung kelipatan 3,5 dengan nilai maksimal 35
+ */
+public class TI_RegPagi22205043Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      for(double kelipatan = 3.5; kelipatan <= 35; kelipatan+= 3.5){
+        System.out.println(kelipatan);
+    }
+        
+    }
+    
+}
